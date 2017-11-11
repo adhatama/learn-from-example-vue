cbc97558618f6a32488019a084ead0e17d2e349a
Adhatama <am.adhatama@gmail.com>
2017-11-11 14:43:21 +0700

    add js-cookie and dummy cookie handler in router

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import VueResource from 'vue-resource'
 import BootstrapVue from 'bootstrap-vue'
+import Cookies from 'js-cookie'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -16,6 +17,23 @@ Vue.config.productionTip = false
 // Set vue-resource configuration
 console.log(process.env.API_HOST)
 Vue.http.options.root = process.env.API_HOST
+
+// Middleware to check auth
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.auth)) {
+    Cookies.set('token', 'hehe')
+    if (!Cookies.get('token')) {
+      next({
+        name: 'login',
+        query: { redirect: to.fullPath }
+      })
+    } else {
+      next()
+    }
+  } else {
+    next() // make sure to always call next()!
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
